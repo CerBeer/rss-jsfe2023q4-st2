5fd694c193ae5760ce9154b63e267016c8dd7040
CerBeer <ocelopp@gmail.com>
2024-02-24 01:09:07 +0300

    feat: add sources and news interface

---
+++ migration-newip-to-ts/src/types/index.ts
@@ -0,0 +1,35 @@
+// Sources
+export interface Source {
+    id: string;
+    name: string;
+    description: string;
+    url: string;
+    category: string;
+    language: string;
+    country: string;
+}
+
+export interface Sources {
+    status: string;
+    articles: Array<Source>;
+}
+
+// Articles
+export interface Article {
+    source: {
+        id: string | null;
+        name: string;
+    };
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+    publishedAt: string;
+    content: string;
+}
+
+export interface Articles {
+    status: string;
+    articles: Array<Article>;
+}
