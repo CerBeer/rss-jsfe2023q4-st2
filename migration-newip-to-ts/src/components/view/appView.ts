a75d365357d2d509fdc5ef429168663c9f96fafb
CerBeer <ocelopp@gmail.com>
2024-02-24 02:35:18 +0300

    feat: adapt to TS file view-appView.js

--- migration-newip-to-ts/src/components/view/appView.ts
+++ migration-newip-to-ts/src/components/view/appView.ts
@@ -1,18 +1,23 @@
 import News from './news/news';
 import Sources from './sources/sources';
+import { sources } from '../../types/index';
+import { articles } from '../../types/index';
 
 export class AppView {
+    news: News;
+    sources: Sources;
+
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
+    drawNews(data: articles) {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    drawSources(data: sources) {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
